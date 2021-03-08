339efd9d49507a6c1629a2e50a115e1d187d5dfb
ricardoslv <ricardoslv80@gmail.com>
2021-03-08 18:25:01 +0000

    Refactor g1 with a stub messaging service

--- g1/src/main/java/TesterAsync.java
+++ g1/src/main/java/TesterAsync.java
@@ -1,15 +1,10 @@
 import java.util.Random;
-import io.atomix.cluster.messaging.MessagingConfig;
-import io.atomix.cluster.messaging.impl.NettyMessagingService;
-import io.atomix.utils.net.Address;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import myutils.AsyncStub;
 
 public class TesterAsync {
 
-  private static void runOperations(ScheduledExecutorService es, NettyMessagingService ms, final int port,
-      final int serverp1, final int serverp2, final int value, final int number) {
+  private static void runOperations(AsyncStub msgService, final int value, final int number) {
 
     System.out.println(number);
     if (number == 0)
@@ -21,27 +16,12 @@ public class TesterAsync {
       // value*[-100,100]
       int accountMovement = (int) (Math.round(50 * (-2.0 + rand.nextFloat() * 4.0)));
 
-      String customType = port + "" + number + " " + operation;
-      // second arg is ignored for balance
-      byte[] operationMsg = (port + "" + number + " " + accountMovement).getBytes();
-
-      Boolean[] active = { true };
-      ms.registerHandler(customType, (address, msg) -> {
-        if (active[0]) {
-          synchronized (active) {
-            active[0] = false;
-          }
-          ms.unregisterHandler(operation);
-          if (operation == "movement" && msg[0] != 0)
-            runOperations(es, ms, port, serverp1, serverp2, value + accountMovement, number - 1);
-          else
-            runOperations(es, ms, port, serverp1, serverp2, value, number - 1);
-        }
-
-      }, es);
-
-      ms.sendAsync(Address.from("localhost", serverp1), operation, operationMsg);
-      ms.sendAsync(Address.from("localhost", serverp2), operation, operationMsg);
+      msgService.sendAndReceiveAsync(number, operation, accountMovement).thenAccept(msg -> {
+        if (operation == "movement" && msg[0] != 0)
+          runOperations(msgService, value + accountMovement, number - 1);
+        else
+          runOperations(msgService, value, number - 1);
+      });
     }
   }
 
@@ -51,16 +31,9 @@ public class TesterAsync {
     int serverp1 = Integer.parseInt(args[1]);
     int serverp2 = Integer.parseInt(args[2]);
     System.out.println("PORTS->" + port + serverp1 + serverp2);
-    ScheduledExecutorService es = Executors.newScheduledThreadPool(1);
-    NettyMessagingService ms = new NettyMessagingService("bank", Address.from(port), new MessagingConfig());
 
-    try {
-      Thread.sleep(100, 0);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    ms.start();
+    AsyncStub msgService = new AsyncStub(port, serverp1, serverp2);
 
-    runOperations(es, ms, port, serverp1, serverp2, 0, 10_000);
+    runOperations(msgService, 0, 10_000);
   }
 }
---
+++ g1/src/main/java/myutils/AsyncStub.java
@@ -0,0 +1,56 @@
+package myutils;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import io.atomix.cluster.messaging.MessagingConfig;
+import io.atomix.cluster.messaging.impl.NettyMessagingService;
+import io.atomix.utils.net.Address;
+
+public class AsyncStub {
+  int port;
+  int serverp1;
+  int serverp2;
+  ScheduledExecutorService es;
+  NettyMessagingService ms;
+
+  public AsyncStub(final int port, final int serverp1, final int serverp2) {
+    this.serverp1 = serverp1;
+    this.serverp2 = serverp2;
+    this.es = Executors.newScheduledThreadPool(1);
+    this.ms = new NettyMessagingService("bank", Address.from(port), new MessagingConfig());
+
+    try {
+      Thread.sleep(100, 0);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    ms.start();
+  }
+
+  public final CompletableFuture<byte[]> sendAndReceiveAsync(final int number, String operation, int accountMovement) {
+
+    CompletableFuture<byte[]> res = new CompletableFuture<byte[]>();
+
+    String customType = port + "" + number + " " + operation;
+    byte[] operationMsg = (port + "" + number + " " + accountMovement).getBytes();
+
+    Boolean[] active = { true };
+    ms.registerHandler(customType, (address, msg) -> {
+      synchronized (active) {
+        if (active[0]) {
+          active[0] = false;
+          ms.unregisterHandler(operation);
+          res.complete(msg);
+        }
+      }
+    }, es);
+
+    ms.sendAsync(Address.from("localhost", serverp1), operation, operationMsg);
+    ms.sendAsync(Address.from("localhost", serverp2), operation, operationMsg);
+
+    return res;
+  }
+}
